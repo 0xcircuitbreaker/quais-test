19e7e136c9bb0f50661489e8a0d45b762ec91177
Alan Orwick <alan.kev11@gmail.com>
2023-02-03 17:07:55 -0600

    Update import

--- scripts/grind-erc20x.ts
+++ scripts/grind-erc20x.ts
@@ -1,7 +1,7 @@
 //load 'quais' and 'fs'
 import { quais } from "quais";
 import { allNodeData } from "./node-data";
-import { allAddressData } from "./address-data"
+import { allAddressData } from "./coinbase-addresses"
 import { getShardFromAddress } from "./shard-data"
 
 const fs = require('fs');
@@ -19,6 +19,7 @@ async function main() {
 
     var sendNodeData = allNodeData[sendAddrData.chain];
         
+    
     // Paste bytecode from etx-compiled after solc run
     var bytecode = "60806040526000806000806000600180620186a06706f05b59d3b20000735a457339697cb56e5a9bfa5267ea80d2c6375d986000f690505060698060446000396000f3fe6080604052600080fdfea264697066735822122048e125ec447a897915e034dd8d5c5295f59dd458bb031f38518bd59acc02c08864736f6c63782c302e382e31382d646576656c6f702e323032322e31312e382b636f6d6d69742e36306161353861362e6d6f64005d";
     //Read abi file to object; names of the solcjs-generated files renamed
@@ -64,7 +65,7 @@ async function main() {
     const myContract = new quais.ContractFactory(abi, toHexString(initCodeBytes), walletWithProvider);
 
     // If your contract requires constructor args, you can specify them here
-    const contract = await myContract.deploy({ gasLimit: 10000000 });
+    const contract = await myContract.deploy({ gasLimit: 99999 });
     
     console.log(contract.address);
     console.log(contract.deployTransaction);
