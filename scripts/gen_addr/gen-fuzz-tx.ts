99c0dca45f490ae4a66d7dd07b488b78d8c168b6
Alan Orwick <alan.kev11@gmail.com>
2023-04-10 17:23:15 -0500

    Added ability to gen keys

---
+++ scripts/gen_addr/gen-fuzz-tx.ts
@@ -0,0 +1,161 @@
+import { quais } from "quais";
+import { allNodeData } from "../node-data";
+import { allAddressData } from "../coinbase-addresses"
+import { addressList } from "../address-list";
+import { typeFlag } from 'type-flag'
+import { getShardFromAddress } from "../shard-data";
+import fs from 'fs';
+
+const parsed = typeFlag({
+    from: {
+        type: String,
+        default: "zone-0-0",
+        alias: "f"
+    },
+    interval: {
+        type: Number,
+        default: 1000,
+        alias: "i"
+    },
+    total: {
+        type: Number,
+        default: 10,
+        alias: "t"
+    },
+    loValue: {
+        type: Number,
+        default: 1,
+        alias: "l"
+    },
+    hiValue: {
+        type: Number,
+        default: 100,
+        alias: "h"
+    },
+    addrList: {
+        type: Boolean,
+        default: false,
+        alias: "a"
+    }
+})
+
+const inputFilePath = 'genWallet.json';
+
+const aggBalances: { [key: string]: number } = {};
+
+async function main() {
+    
+    let from = parsed.flags.from
+    let interval = parsed.flags.interval
+    let total = parsed.flags.total
+    let loValue = parsed.flags.loValue
+    let hiValue = parsed.flags.hiValue
+    let addrList = parsed.flags.addrList
+
+    logArgs(from, interval, total, loValue, hiValue, addrList)
+    
+    var sendAddrData = allAddressData[from];
+    if (sendAddrData == undefined) {
+        console.log("Sending address not provided");
+        return;
+    }
+
+    var sendNodeData = allNodeData[sendAddrData.chain];
+
+    const provider = new quais.providers.JsonRpcProvider(sendNodeData.provider);
+
+
+    const genWallet = await fs.promises.readFile(inputFilePath, 'utf8')
+
+    let wallet = JSON.parse(genWallet);
+    let shardKey = wallet[sendAddrData.chain as any];
+    console.log(shardKey);
+    let privKey = quais.utils.arrayify(shardKey.privateKey);
+    console.log(privKey);
+    const walletWithProvider = new quais.Wallet(privKey, provider);
+
+    await provider.ready;
+
+    console.log("Sending Address: ", walletWithProvider.address)
+
+    var nonce = await provider.getTransactionCount(walletWithProvider.address);
+    for(var i = 0; i < total; i++) {
+        var value = Math.floor(Math.random() * (hiValue - loValue + 1) + loValue);
+
+        const balanace = await provider.getBalance(walletWithProvider.address);
+        console.log("Balance: ", Number(balanace));
+        if(Number(balanace) < value) {
+            console.log("Insufficient balance");
+            return;
+        }
+        var receiveAddr;
+        if(addrList) {
+            receiveAddr = addressList[Math.floor(Math.random() * addressList.length)];
+        } else {
+            var receiveData = Object.keys(allAddressData)[Math.floor(Math.random() * Object.keys(allAddressData).length)];
+            receiveAddr = allAddressData[receiveData].address;
+        }
+        await sendTx(value, receiveAddr, sendAddrData, walletWithProvider, nonce);
+        await sleep(interval);
+        nonce++;
+    }
+    console.log("Aggregated Balances: ", aggBalances);
+}
+
+async function sendTx(value: number, toAddress: string, sendAddrData: any, walletWithProvider: any, nonce: number) {
+    var txData = {
+        to: toAddress,
+        from: sendAddrData.address,
+        value: value,
+    } as quais.providers.TransactionRequest;
+
+    let shardFrom = getShardFromAddress(sendAddrData.address)[0];
+    let shardTo = getShardFromAddress(toAddress)[0];
+
+    console.log("toAddress: ", toAddress, " shardTo: ", shardTo)
+    console.log("From: ", shardFrom, " To: ", shardTo, " Value: ", value)
+    var feeData = await walletWithProvider.getFeeData() 
+    console.log("Fee Data: ", Number(feeData.maxFeePerGas), Number(feeData.maxPriorityFeePerGas))
+    if(shardFrom != shardTo) {
+        txData = {
+            to: toAddress,
+            from: sendAddrData.address,
+            value: value,
+            externalGasLimit: 110000,
+            externalGasPrice:  Number(feeData.maxFeePerGas) * 2,
+            externalGasTip:  Number(feeData.maxPriorityFeePerGas) * 2,
+            gasLimit: 100000,
+            maxFeePerGas: Number(feeData.maxFeePerGas),
+            maxPriorityFeePerGas: Number(feeData.maxPriorityFeePerGas),
+            type: 2,
+            nonce: nonce,
+        };
+    }
+
+    try {
+        const tx = await walletWithProvider.sendTransaction(txData);
+        console.log("Transaction sent", tx)
+        if(aggBalances[toAddress] == undefined) {
+            aggBalances[toAddress] = 0;
+        }
+        aggBalances[toAddress] += Number(value);
+        } catch (e: any) {
+            console.log(e.reason);
+        }
+        
+}
+
+function sleep(s: number) {
+    return new Promise(resolve => setTimeout(resolve, s));
+}
+
+function logArgs(from: string, interval: number, total: number, loValue: number, hiValue: number, randomize: boolean) {
+    console.log("From: ", from);
+    console.log("Interval: ", interval);
+    console.log("Total: ", total);
+    console.log("Low Value: ", loValue);
+    console.log("High Value: ", hiValue);
+    console.log("Randomize: ", randomize);
+}
+
+main();
---
+++ scripts/gen_addr/generate_addresses.js
@@ -0,0 +1,140 @@
+// import * as bip39 from "bip39"
+const { getShardFromAddress, SigningKey, computeAddress } = require("quais/lib/utils")
+const { HDKey } = require("@scure/bip32")
+const {validateMnemonic, generateMnemonic, mnemonicToSeedSync} = require('bip39')
+const fs = require('fs')
+const { quais } = require("quais")
+
+// Options:
+const defaultHDPath = `m/44'/994'/0'/0`
+
+/**
+ * Sets appropriate properties for the keyring based on the given
+ * BIP39-compliant mnemonic.
+ *
+ * @param {string|Array<number>|Buffer} mnemonic - A seed phrase represented
+ * as a string, an array of UTF-8 bytes, or a Buffer. Mnemonic input
+ * passed as type buffer or array of UTF-8 bytes must be NFKD normalized.
+ */
+function getWalletFromMnemonic(mnemonic) {
+    // validate before initializing
+    const isValid = validateMnemonic(mnemonic)
+    if (!isValid) {
+      throw new Error("Hd-Keyring: Invalid secret recovery phrase provided")
+    }
+  
+    // eslint-disable-next-line node/no-sync
+    const seed = mnemonicToSeedSync(mnemonic)
+    return HDKey.fromMasterSeed(seed)
+  }
+
+  
+
+/**
+ * Returns a derived address from a given HDKey. Options available
+ * are path (defaults to the Quai 994 path) and index (defaults to 0).
+ *
+ * @param HDKey
+ * @param opts
+ * @returns
+ */
+function deriveAddress(HDKey, opts) {
+    let path = opts.path || defaultHDPath
+    let index = opts.index || 0
+  
+    let childKey = HDKey.derive(path + "/" + index.toString())
+    let signingKey = new SigningKey(childKey.privateKey)
+    let address = computeAddress(signingKey.publicKey)
+    return address
+}
+
+
+function grindAddress(keyfile, path, index, shard) {
+    let found = false
+    let newAddress = ""
+    while (!found) {
+      newAddress = deriveAddress(keyfile, { hdPath: path, index: index })
+      let addrShard = getShardFromAddress(newAddress)
+        // Check if address is in a shard
+      if (addrShard !== undefined) {
+        // Check if address is in correct shard
+        if (addrShard === shard) {
+          found = true
+          break
+        }
+      }
+      index++
+    }
+    return {
+      address: newAddress,
+      index: index,
+      path: path + "/" + index.toString(),
+      privateKey: quais.utils.hexlify(keyfile.privateKey)
+    }
+}
+
+    
+async function generateRandomMnemonic() {
+return await generateMnemonic()
+}
+
+const shards = [
+    "prime",
+    "region-0",
+    "zone-0-0",
+    "zone-0-1",
+    "zone-0-2",
+    "region-1",
+    "zone-1-0",
+    "zone-1-1",
+    "zone-1-2",
+    "region-2",
+    "zone-2-0",
+    "zone-2-1",
+    "zone-2-2",
+]
+
+const shardToEnv = {
+    "prime": "PRIME_COINBASE",
+    "region-0": "REGION_0_COINBASE",
+    "zone-0-0": "ZONE_0_0_COINBASE",
+    "zone-0-1": "ZONE_0_1_COINBASE",
+    "zone-0-2": "ZONE_0_2_COINBASE",
+    "region-1": "REGION_1_COINBASE",
+    "zone-1-0": "ZONE_1_0_COINBASE",
+    "zone-1-1": "ZONE_1_1_COINBASE",
+    "zone-1-2": "ZONE_1_2_COINBASE",
+    "region-2": "REGION_2_COINBASE",
+    "zone-2-0": "ZONE_2_0_COINBASE",
+    "zone-2-1": "ZONE_2_1_COINBASE",
+    "zone-2-2": "ZONE_2_2_COINBASE",
+}
+
+
+const genWallet = {};
+
+const outputFilePath = 'genWallet.json';
+const envFilePath = 'network.env';
+
+async function main() {
+    let mnemonic = await generateRandomMnemonic()
+    let keyfile = getWalletFromMnemonic(mnemonic)
+
+
+    for (let shard of shards) {
+        let address = grindAddress(keyfile, defaultHDPath, 0, shard)
+        genWallet[shard] = address
+        fs.appendFile(envFilePath, `${shardToEnv[shard]}=${address.address}\n`, function (err) {
+          if (err) throw err;
+        });
+    }
+    fs.writeFile(outputFilePath, JSON.stringify(genWallet, null, 2), (err) => {
+      if (err) {
+        console.error('Error writing file:', err);
+      } else {
+        console.log(`genWallet saved to ${outputFilePath}`);
+      }
+    });
+}
+
+main()
